308618c8e289604f2ca6b11e79723b938f3bddcf
Charvick-Nuvusetty <69137274+Charvick-Nuvusetty@users.noreply.github.com>
2020-10-15 18:54:51 +0530

    Statistical Inference Using R

---
+++ PDF.R
@@ -0,0 +1,12 @@
+#Probability dense function
+
+x<-c(-0.5,0,1,1,1.5)
+y<-c(0,0,2,0,0)
+plot(x,y,lwd=3,frame =TRUE,type="l")
+
+#It is a valid probability density function  since it is > 0 and the area of the result triangle is equal to 1 
+
+1.5*0.75/2
+
+pbeta(0.75,2,1)#to calculate the probablity from a beta density
+
